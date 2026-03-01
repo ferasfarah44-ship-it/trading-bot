8e678bf1c2694a1a21e6c3dc70b137dbf01ffdeb
ferasfarah44-ship-it <ferasfarah44@gmail.com>
2026-03-01 20:42:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,6 @@
-import requests
+print("START")
+
 import time
-
-BOT_TOKEN = "PUT_YOUR_TOKEN"
-CHAT_ID = "PUT_YOUR_CHAT_ID"
-
-def send_telegram(msg):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
-
-print("BOOTING...")
-
-send_telegram("🚀 البوت اشتغل الآن")
-
 while True:
-    print("RUNNING LOOP")
-    time.sleep(30)
+    print("ALIVE")
+    time.sleep(5)
