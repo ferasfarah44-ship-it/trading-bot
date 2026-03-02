542185cb756b50a6120a4f270c4249538fb4355f
ferasfarah44-ship-it <ferasfarah44@gmail.com>
2026-03-03 02:18:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import os
+import requests
+
 print("BOT STARTED...")
-print("Running main file")
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+data = {
+    "chat_id": CHAT_ID,
+    "text": "البوت اشتغل بنجاح ✅"
+}
+
+response = requests.post(url, data=data)
+print("Telegram response:", response.text)
