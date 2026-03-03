425afc627d85ee6c20c6f29c5234306c5edfdde7
ferasfarah44-ship-it <ferasfarah44@gmail.com>
2026-03-03 19:49:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,30 @@
 import os
 import telebot
 import time
+import requests
 
 TOKEN = os.getenv("BOT_TOKEN")
-
 bot = telebot.TeleBot(TOKEN, parse_mode="HTML")
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.reply_to(message, "🚀 البوت يعمل بثبات على Railway")
+    bot.reply_to(message, "🚀 البوت يعمل بثبات 24/7")
 
-def run():
+def run_bot():
     while True:
         try:
-            bot.remove_webhook()
-            time.sleep(1)
-            print("Bot started polling...")
+            print("Starting polling...")
             bot.infinity_polling(
                 timeout=60,
                 long_polling_timeout=60,
                 skip_pending=True
             )
+        except requests.exceptions.ConnectionError as e:
+            print("Connection lost. Retrying in 5 sec...")
+            time.sleep(5)
         except Exception as e:
-            print("Polling error:", e)
+            print("Unexpected error:", e)
             time.sleep(5)
 
 if __name__ == "__main__":
-    run()
+    run_bot()
